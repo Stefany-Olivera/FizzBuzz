123b3bd191c10c271251e744c4426be130e2b5e9
Stefany Olivera <34490162+Stefany-Olivera@users.noreply.github.com>
2018-04-24 17:04:49 -0400

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,14 @@
+public class FizzBuzz {
+    public String fizzBuzz(int number) {
+        // fizzbuzz logic here
+        if(number % 3 == 0 && number % 5 != 0){
+            return "Fizz";
+        }else if(number % 5 == 0 && number % 3 != 0){
+            return "Buzz";
+        }else if(number % 5 == 0 && number % 3 == 0){
+            return "FizzBuzz";
+        }else{
+            return Integer.toString(number);
+        }
+    }
+}
---
+++ FizzBuzzTest.java
@@ -0,0 +1,9 @@
+public class FizzBuzzTest{
+    public static void main(String[] args){
+        FizzBuzz fb = new FizzBuzz();
+
+        for(int i=0;i<99;i++){
+            System.out.println(fb.fizzBuzz(i));
+        }
+    }
+}
